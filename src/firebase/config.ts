2d1bced223fb996c88d673f8cc4817b07f407a5c
Gungun Aggarwal <gungun.aggarwal@delhitechnicalcampus.ac.in>
2026-02-25 06:34:25 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8242414053-13af1",
+  "appId": "1:241267630558:web:a92bd67afede741463fd4f",
+  "apiKey": "AIzaSyD2A79qCBogTDd0KHeczkzSyQ4a-j1696I",
+  "authDomain": "studio-8242414053-13af1.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "241267630558"
+};
